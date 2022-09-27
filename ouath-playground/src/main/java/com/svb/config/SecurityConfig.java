6032f68a3675d6379211fad2076559b2afd9861d
Zartab Nakhwa <zartab@codewithz.com>
2022-09-27 12:40:47 +0530

    Iteration 2 for Oauth

--- ouath-playground/src/main/java/com/svb/config/SecurityConfig.java
+++ ouath-playground/src/main/java/com/svb/config/SecurityConfig.java
@@ -1,13 +1,8 @@
 package com.svb.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
 @Configuration
 public class SecurityConfig  extends WebSecurityConfigurerAdapter{
@@ -23,26 +18,26 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 	}
 	
 	
-	private ClientRegistration clientRegistration() {
-		
-		ClientRegistration registration= CommonOAuth2Provider.GITHUB
-										.getBuilder("github")
-										.clientId("ee2ad6dc257248a3f5d1")
-										.clientSecret("e64c9ab2f9c75d8aedb194cc052b3039a339fe83")
-										.build();
-		
-		return registration;
-	}
-	
-	@Bean
-	public ClientRegistrationRepository clientRepository() {
-		
-		ClientRegistration registration=clientRegistration();
-		InMemoryClientRegistrationRepository repository=
-						new InMemoryClientRegistrationRepository(registration);
-		
-		return repository;
-	
-	}
+//	private ClientRegistration clientRegistration() {
+//		
+//		ClientRegistration registration= CommonOAuth2Provider.GITHUB
+//										.getBuilder("github")
+//										.clientId("ee2ad6dc257248a3f5d1")
+//										.clientSecret("e64c9ab2f9c75d8aedb194cc052b3039a339fe83")
+//										.build();
+//		
+//		return registration;
+//	}
+//	
+//	@Bean
+//	public ClientRegistrationRepository clientRepository() {
+//		
+//		ClientRegistration registration=clientRegistration();
+//		InMemoryClientRegistrationRepository repository=
+//						new InMemoryClientRegistrationRepository(registration);
+//		
+//		return repository;
+//	
+//	}
 
 }
